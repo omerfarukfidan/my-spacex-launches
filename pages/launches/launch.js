c8885536d78ad6911cf0683108419102c48ded8e
omerfarukfidan <fidanomerfaruk01@gmail.com>
2023-07-26 18:32:54 +0300

    example of fething api

--- pages/launches/launch.js
+++ pages/launches/launch.js
@@ -1,13 +1,41 @@
 import Link from "next/link";
 import Layout from "../../components/layout";
-export default function () {
+import { useEffect, useState } from "react";
+
+const Launches = () => {
+  const [userInfo, setUserInfo] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const query = await fetch("https://jsonplaceholder.typicode.com/users");
+      const response = await query.json();
+      console.log("response from api", response);
+      setUserInfo(response);
+    };
+    getData();
+  }, []);
+
   return (
     <>
       <Layout>
         <div>
           <h2> This is Launches Page! </h2>
         </div>
+
+        <div>
+          {userInfo &&
+            userInfo.length &&
+            userInfo.map((user, index) => {
+              console.log(index);
+              return (
+                <>
+                  <h4 key={user.id}>{user.name}</h4>
+                </>
+              );
+            })}
+        </div>
       </Layout>
     </>
   );
-}
+};
+
+export default Launches;
